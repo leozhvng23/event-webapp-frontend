980a1e20ba7ca2a44d9fd10b1d279a20fed9cf5f
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2023-04-14 00:51:18 -0400

    displays user db doc

--- src/user/pages/ProfilePage.js
+++ src/user/pages/ProfilePage.js
@@ -1,21 +1,65 @@
 import React, { useState, useContext, useEffect } from "react";
 import AuthContext from "../../common/context/AuthContext";
+import { getUser } from "../../common/api/user";
 
 const ProfilePage = () => {
   const { currentUser, isLoggedIn } = useContext(AuthContext);
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      const headers = {
+        "Content-Type": "application/json",
+        "x-api-key": process.env.REACT_APP_API_KEY,
+      };
+      let authToken = null;
+
+      if (currentUser && currentUser.signInUserSession) {
+        authToken = currentUser.signInUserSession.idToken.jwtToken;
+      }
+      console.log("authToken:", authToken);
+
+      try {
+        const response = await getUser(currentUser.attributes.sub, authToken);
+        setUserData(response);
+        console.log("response:", response);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (isLoggedIn) {
+      fetchData();
+    }
+  }, [currentUser, isLoggedIn]);
 
   return (
     <div>
       <h2>Profile Page</h2>
       {isLoggedIn ? (
-        <div>
-          <h3>User Information</h3>
-          <p>Name: {currentUser.attributes.name}</p>
-          <p>Email: {currentUser.attributes.email}</p>
-          <p>Username: {currentUser.username}</p>
-          <p>Id: {currentUser.attributes.sub}</p>
-          <p>Id Token: {currentUser.signInUserSession.idToken.jwtToken}</p>
-        </div>
+        loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            <h3>User Information</h3>
+            {userData && (
+              <>
+                <p>Name: {userData.name}</p>
+                <p>Email: {userData.email}</p>
+                <p>Username: {userData.username}</p>
+                <p>Id: {userData.id}</p>
+                <p>Bio: {userData.bio}</p>
+              </>
+            )}
+            <h4>ID Token:</h4>
+            <pre>{currentUser.signInUserSession.idToken.jwtToken}</pre>
+          </div>
+        )
       ) : (
         <p>Please log in to view your profile.</p>
       )}
