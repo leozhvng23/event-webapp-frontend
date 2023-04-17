6d515ef00d05cf292cad6146e2a7c6f315f861a3
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2023-04-16 22:56:58 -0400

    delete image

--- src/common/api/s3.js
+++ src/common/api/s3.js
@@ -19,6 +19,17 @@ export const uploadImageToS3 = async (imageFile, eventId, folder) => {
   }
 };
 
+export const deleteImageFromS3 = async (key) => {
+  try {
+    await Storage.remove(key);
+    console.log("Successfully deleted image from S3");
+    return true;
+  } catch (error) {
+    console.error("Error deleting image from S3:", error);
+    return false;
+  }
+};
+
 export const getImageURL = async (key) => {
   try {
     const signedURL = await Storage.get(key);
