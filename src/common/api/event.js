976a9479bc1e29485e00f7f710f21b41fb226709
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2023-04-16 22:56:44 -0400

    get event by id and update event

--- src/common/api/event.js
+++ src/common/api/event.js
@@ -38,10 +38,54 @@ export const getUserEvents = async (userId, authToken) => {
     const response = await API.get("APIGatewayAPI", `/user/${userId}/events`, {
       headers,
     });
-    // check for success and return response
     console.log("success fetching user events:", response);
     return response;
   } catch (error) {
     throw new Error("Error fetching user events:", error);
+  }
+};
+
+export const getEventById = async (eventId, authToken) => {
+  const headers = {
+    "Content-Type": "application/json",
+    "x-api-key": process.env.REACT_APP_API_KEY,
+    Authorization: authToken,
+  };
+
+  try {
+    console.log("headers:", headers);
+    console.log("path parameter id: ", eventId);
+    const response = await API.get("APIGatewayAPI", `/event/${eventId}`, {
+      headers,
+    });
+    console.log("success fetching event by id:", response);
+    return response;
+  } catch (error) {
+    throw new Error("Error fetching event by id:", error);
+  }
+};
+
+export const updateEvent = async (userId, eventId, eventData, authToken) => {
+  const headers = {
+    "Content-Type": "application/json",
+    "x-api-key": process.env.REACT_APP_API_KEY,
+  };
+  headers.Authorization = authToken;
+  const body = eventData;
+  try {
+    console.log("headers:", headers);
+    console.log("request body:", body);
+    const response = await API.put("APIGatewayAPI", `/user/${userId}/event/${eventId}`, {
+      headers,
+      body,
+    });
+    if (response.statusCode === 200) {
+      console.log("success updating event:", response);
+      return response;
+    } else if (response.statusCode === 400) {
+      throw new Error("Error updating event: invalid input");
+    }
+  } catch (error) {
+    throw new Error("Error updating event:", error);
   }
 };
