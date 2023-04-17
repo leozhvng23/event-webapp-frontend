68f137be2f77b7535e36c4daf5b6a6f5554c28f9
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2023-04-16 22:56:14 -0400

    reformat Date and Time

--- src/common/util/formatInput.js
+++ src/common/util/formatInput.js
@@ -2,3 +2,20 @@ export const combineDateAndTime = (date, time) => {
   const dateTime = new Date(date + "T" + time);
   return dateTime.toISOString();
 };
+
+export const reformatDate = (dateTime) => {
+  const date = new Date(dateTime);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+};
+
+export const reformatTime = (dateTime) => {
+  const date = new Date(dateTime);
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${hours}:${minutes}`;
+};
