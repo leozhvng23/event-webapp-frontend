43c44a17610b028cfd5ec28a7ad9f68d008366cc
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2023-04-24 00:05:02 -0400

    displays location

--- src/event/components/EventCard.js
+++ src/event/components/EventCard.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { getImageURL } from "../../common/api/s3";
 import { formatDate, formatTime } from "../../common/util/formatOutput";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendar, faClock } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar, faClock, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 const EventCard = ({ event }) => {
   console.log("EventCard", event.id);
@@ -22,20 +22,45 @@ const EventCard = ({ event }) => {
     }
   }, [event.image]);
 
+  const getLocationLabel = (location) => {
+    if (location.neighborhood) {
+      return location.neighborhood;
+    } else if (location.municipality) {
+      return location.municipality;
+    } else if (location.region) {
+      return location.region;
+    } else if (location.country) {
+      return location.country;
+    } else {
+      return null;
+    }
+  };
+
+  const locationLabel = getLocationLabel(event.location);
+
   return (
     <Link to={`/event/${event.id}`} className="block">
-      <div className="bg-white shadow-md rounded-md overflow-hidden max-w-sm mx-auto my-6">
+      <div className="bg-white shadow-md rounded-md overflow-hidden max-w-md m-auto">
         {imageURL && (
           <img className="w-full h-64 object-cover" src={imageURL} alt={event.name} />
         )}
         <div className="p-4">
           <h3 className="text-xl font-semibold mb-2">{event.name}</h3>
-          <p className="text-gray-600">{event.description}</p>
+          <p className="text-gray-600 line-clamp-1">{event.description}</p>
           <div className="flex items-center mt-2">
             <FontAwesomeIcon icon={faCalendar} className="text-gray-600 mr-2" />
             <span className="text-gray-600">{formatDate(event.dateTime)}</span>
             <FontAwesomeIcon icon={faClock} className="text-gray-600 ml-4 mr-2" />
             <span className="text-gray-600">{formatTime(event.dateTime)}</span>
+            {locationLabel && (
+              <>
+                <FontAwesomeIcon
+                  icon={faMapMarkerAlt}
+                  className="text-gray-600 ml-4 mr-2"
+                />
+                <span className="text-gray-600 line-clamp-1">{locationLabel}</span>
+              </>
+            )}
           </div>
         </div>
       </div>
