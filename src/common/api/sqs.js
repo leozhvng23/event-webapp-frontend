652f49293b589c209efac665e2967c499f7e2226
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2023-04-25 16:25:04 -0400

    moved aws config update inside longPollQueue func

--- src/common/api/sqs.js
+++ src/common/api/sqs.js
@@ -1,14 +1,15 @@
 import AWS from "aws-sdk";
 
-AWS.config.update({
-  region: process.env.REACT_APP_AWS_REGION,
-  accessKeyId: process.env.REACT_APP_SQS_ACCESS_KEY,
-  secretAccessKey: process.env.REACT_APP_SQS_SECRET_ACCESS_KEY,
-});
-const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
 const queueUrl = "https://sqs.us-east-1.amazonaws.com/612277434742/Eventful-Invitations";
 
 export const longPollQueue = async (userId, onMessagesReceived, onError) => {
+  AWS.config.update({
+    region: process.env.REACT_APP_AWS_REGION,
+    accessKeyId: process.env.REACT_APP_SQS_ACCESS_KEY,
+    secretAccessKey: process.env.REACT_APP_SQS_SECRET_ACCESS_KEY,
+  });
+
+  const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
   let continuePolling = true;
 
   const stopPolling = () => {
