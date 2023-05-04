cea7679d57417d5f906faa88729a88bd9fc26a9f
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2023-05-04 15:59:27 -0400

    updateInvitation

--- src/common/api/invitation.js
+++ src/common/api/invitation.js
@@ -49,3 +49,28 @@ export const getInvitationsByEventId = async (eid, authToken) => {
     throw new Error("Error fetching invitations:", error);
   }
 };
+
+export const updateInvitation = async (requestBody, authToken) => {
+  const headers = {
+    "Content-Type": "application/json",
+    "x-api-key": process.env.REACT_APP_API_KEY,
+    Authorization: authToken,
+  };
+  const body = requestBody;
+  try {
+    console.log("headers:", headers);
+    console.log("request body:", body);
+    const response = await API.put("APIGatewayAPI", "/invitation", {
+      headers,
+      body,
+    });
+    if (response.statusCode === 200) {
+      console.log("success updating invitation:", response);
+      return response;
+    } else if (response.statusCode === 400) {
+      throw new Error("Error updating invitation: invalid input");
+    }
+  } catch (error) {
+    throw new Error("Error updating invitation:", error);
+  }
+};
