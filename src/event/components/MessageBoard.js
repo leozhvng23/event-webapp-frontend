4728cd3ad5dbd5dca075f9885d8c2f7288591e5d
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2023-05-05 14:35:49 -0400

    style update

--- src/event/components/MessageBoard.js
+++ src/event/components/MessageBoard.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { formatTimestamp } from "../../common/util/formatTimestamp";
 import MessageInput from "./MessageInput";
 
-const MessageBoard = ({ messages, currentUserId, onSendMessage }) => {
+const MessageBoard = ({ messages, currentUserId, onSendMessage, onRefresh }) => {
   const messagesContainerRef = useRef(null);
 
   useEffect(() => {
@@ -14,41 +14,53 @@ const MessageBoard = ({ messages, currentUserId, onSendMessage }) => {
 
   return (
     <div className="flex flex-col h-[295px] justify-between">
-      <div ref={messagesContainerRef} className="flex-grow overflow-y-auto px-4">
-        {messages.map((message) => {
-          const isCurrentUser = message.uid === currentUserId;
+      <div
+        ref={messagesContainerRef}
+        className="flex-grow overflow-y-auto px-4 bg-gray-100 rounded-lg shadow-inner"
+      >
+        {messages.length === 0 ? (
+          <div className="flex items-center justify-center h-full">
+            <span className="text-gray-500 text-lg">Be the first to post a message!</span>
+          </div>
+        ) : (
+          <>
+            <div className="h-2"></div>
+            {messages.map((message) => {
+              const isCurrentUser = message.uid === currentUserId;
 
-          return (
-            <div key={message.id} className="mb-2">
-              <div
-                className={`px-4 py-2 rounded-xl ${
-                  isCurrentUser
-                    ? "bg-blue-500 text-white float-right"
-                    : "bg-gray-300 text-black float-left"
-                }`}
-                style={{ maxWidth: "80%" }}
-              >
-                <div className="whitespace-pre-wrap">{message.content}</div>
-              </div>
-              <div className="clear-both"></div>
-              <div
-                className={`text-xs text-gray-500 px-1 mt-[2px] flex ${
-                  isCurrentUser ? "justify-end" : "justify-start"
-                }`}
-              >
-                <Link
-                  to={`/profile/${message.uid}`}
-                  className="mr-2 font-semibold text-gray-500 hover:text-blue-600"
-                >
-                  {message.name}
-                </Link>
-                <span>{formatTimestamp(message.timestamp)}</span>
-              </div>
-            </div>
-          );
-        })}
+              return (
+                <div key={message.id} className="mb-2">
+                  <div
+                    className={`px-4 py-2 rounded-xl ${
+                      isCurrentUser
+                        ? "bg-blue-500 text-white float-right"
+                        : "bg-gray-300 text-black float-left"
+                    }`}
+                    style={{ maxWidth: "80%" }}
+                  >
+                    <div className="whitespace-pre-wrap">{message.content}</div>
+                  </div>
+                  <div className="clear-both"></div>
+                  <div
+                    className={`text-xs text-gray-500 px-1 mt-[2px] flex ${
+                      isCurrentUser ? "justify-end" : "justify-start"
+                    }`}
+                  >
+                    <Link
+                      to={`/profile/${message.uid}`}
+                      className="mr-2 font-semibold text-gray-500 hover:text-blue-600"
+                    >
+                      {message.name}
+                    </Link>
+                    <span>{formatTimestamp(message.timestamp)}</span>
+                  </div>
+                </div>
+              );
+            })}
+          </>
+        )}
       </div>
-      <MessageInput onSendMessage={onSendMessage} />
+      <MessageInput onSendMessage={onSendMessage} onRefresh={onRefresh} />
     </div>
   );
 };
