25bff822a83821462e884805a9fa67a4c21a501a
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2023-05-05 13:47:00 -0400

    message board

--- src/event/pages/EventPage.js
+++ src/event/pages/EventPage.js
@@ -12,11 +12,14 @@ import Loading from "../../common/components/UIElements/Loading";
 import InvitedUsers from "../components/InvitedUsers";
 import { createInvitation } from "../../common/api/invitation";
 import { getInvitationsByEventId, updateInvitation } from "../../common/api/invitation";
+import { getCommentsByEventId, createComment } from "../../common/api/comments";
 import InfoModal from "../components/InfoModal";
 import LoadingModal from "../../common/components/UIElements/LoadingModal";
+import MessageBoard from "../components/MessageBoard";
+import { generateUUID } from "../../common/util/generateId";
 
-// import dummy users data json
 // import usersData from "../../data/dummyInvitedUsers.json";
+// import commentsData from "../../data/dummyComments.json";
 
 const EventPage = () => {
   const { currentUser } = useContext(AuthContext);
@@ -30,6 +33,7 @@ const EventPage = () => {
   const [rsvpStatus, setRsvpStatus] = useState("");
   const [rsvpSending, setRsvpSending] = useState(false);
   const [loadingMessage, setLoadingMessage] = useState("Sending RSVP...");
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,8 +43,10 @@ const EventPage = () => {
       try {
         const fetchedEvent = await getEventById(eventId, authToken);
         const fetchedInvitations = await getInvitationsByEventId(eventId, authToken);
+        const fetchedComments = await getCommentsByEventId(eventId, authToken);
         setInvitedUsers(fetchedInvitations);
         setEvent(fetchedEvent);
+        setComments(fetchedComments);
         setPageLoading(false);
         if (fetchedEvent.image) {
           const url = await getImageURL(fetchedEvent.image);
@@ -94,6 +100,29 @@ const EventPage = () => {
       fetchData();
     }
   }, [currentUser, eventId, rsvpStatus]);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      const authToken = currentUser.signInUserSession.idToken.jwtToken;
+      try {
+        console.log("polling comments");
+        const fetchedComments = await getCommentsByEventId(eventId, authToken);
+        setComments(fetchedComments);
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+      }
+    };
+    if (!currentUser) {
+      return;
+    }
+    const intervalId = setInterval(() => {
+      fetchComments();
+    }, 300000); // 5 minutes
+    // Clean up the interval when the component is unmounted or the user logs out
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [currentUser, eventId]);
 
   const handleEventUpdated = (updatedEvent) => {
     // copy event object and update the fields that are present in the updatedEvent
@@ -184,6 +213,39 @@ const EventPage = () => {
     }
   };
 
+  const handleSendMessage = async (message) => {
+    console.log("message:", message);
+    const commentId = generateUUID();
+    const newComment = {
+      id: commentId,
+      eid: eventId,
+      uid: currentUser.attributes.sub,
+      content: message,
+      name: currentUser.attributes.name,
+      timestamp: new Date().toISOString(),
+    };
+    setComments([...comments, newComment]);
+    const authToken = currentUser.signInUserSession.idToken.jwtToken;
+    const requestBody = {
+      id: commentId,
+      eid: eventId,
+      uid: currentUser.attributes.sub,
+      content: message,
+    };
+    try {
+      const result = await createComment(requestBody, authToken);
+      if (result) {
+        console.log("comment sent successfully!");
+      } else {
+        console.log("Error sending comment.");
+        // delete last comment from comments array
+        setComments(comments.slice(0, -1));
+      }
+    } catch (error) {
+      console.error("Error sending comment:", error);
+    }
+  };
+
   if (pageLoading) {
     return (
       <div className="container mx-auto px-4 max-w-4xl pt-10 flex justify-center">
@@ -225,6 +287,11 @@ const EventPage = () => {
         <Tile className="w-full mb-6 h-auto">
           <div className="p-4">
             <h2 className="text-xl font-semibold mb-4">Message Board</h2>
+            <MessageBoard
+              messages={comments}
+              currentUserId={currentUser.attributes.sub}
+              onSendMessage={handleSendMessage}
+            />
           </div>
         </Tile>
         <Tile className="w-full h-fit mb-6">
