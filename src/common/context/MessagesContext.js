9c3e08e249523a3608846f97986b7ebf691fd866
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2023-05-08 03:29:09 -0400

    sort message by time

--- src/common/context/MessagesContext.js
+++ src/common/context/MessagesContext.js
@@ -6,9 +6,12 @@ const MessagesContext = createContext();
 const messagesReducer = (state, action) => {
   switch (action.type) {
     case "ADD_MESSAGE":
-      return [...state, action.payload];
+      const parsedMessage = { ...action.payload, Body: JSON.parse(action.payload.Body) };
+      return [...state, parsedMessage].sort(
+        (a, b) => new Date(b.Body.timestamp) - new Date(a.Body.timestamp)
+      );
     case "REMOVE_MESSAGE":
-      return state.filter((message) => message.id !== action.payload);
+      return state.filter((message) => message.MessageId !== action.payload);
     default:
       return state;
   }
